48ecd79ba15814c4a969655ed56c8ad5b0f916e8
Fernando Bearzotti <fbearzotti@gmail.com>
2015-08-24 16:46:31 -0300

    24-08-2015

--- Cliente/www/scripts/app.js
+++ Cliente/www/scripts/app.js
@@ -3,16 +3,34 @@
   'categoriasControllers'
 ]);
 
+$(document).ready(function () {
+  inicializoMenu();
+});
+
+
+//SSDApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+//  $routeProvider.
+//      when('/categorias', {
+//        templateUrl: 'partials/categorias.html',
+//        controller: 'categoriasCtrl'
+//      }).
+//      when('/login', {
+//        redirectTo: 'login.html'
+//      }).
+//      otherwise({
+//        redirectTo: 'main.html'
+//      });
+//}]);
+
 SSDApp.config(['$routeProvider',
   function ($routeProvider) {
     $routeProvider.
-      when('/:categorias', {
+      when('/categorias', {
         templateUrl: 'partials/categorias.html',
         controller: 'categoriasCtrl'
       }).
-      when('/main/:main', {
-        templateUrl: 'main.html',
-        controller: 'categoriasCtrl'
+      when('/login', {
+        redirectTo: 'login.html'
       }).
       //when('/phones/:phoneId', {
       //  templateUrl: 'partials/phone-detail.html',
@@ -22,4 +40,38 @@ SSDApp.config(['$routeProvider',
         redirectTo: 'main.html'
       });
   }]);
+
+function inicializoMenu() {
+  FooNav.init({
+    classes: 'fon-rounded fon-shadow',
+    continer: '#mi_menu',
+    position: 'fon-top-right',
+    theme: 'fon-light',
+    items: [{
+      href: 'main.html',
+      text: 'Principal'
+    },
+    {
+      href: '#categorias',
+      text: 'Listado Categorias'
+    },
+    {
+      href: '#entrenamientos',
+      text: 'Entrenamientos',
+      children: [{
+        href: 'main.html?categorias',
+        text: 'Listado Entrenamientos',
+      }, {
+        href: 'main.html?categorias',
+        text: 'Toma de Presentismo'
+      }]
+    },
+    {
+      href: 'main.html#/login',
+      text: 'Ingreso al sistema'
+    }]
+  });
+
+}
+
 
