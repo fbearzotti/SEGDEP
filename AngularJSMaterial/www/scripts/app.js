b565a14df2e7b822047ebeb01b188c52979bc1df
Fernando Bearzotti <fbearzotti@gmail.com>
2015-08-31 16:57:56 -0300

    31-08-2015

---
+++ AngularJSMaterial/www/scripts/app.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('SSDApp', ['ngMaterial']);
+
+app.config(function ($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('indigo')
+    .accentPalette('orange');
+});
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
+  $scope.toggleSidenav = function (menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+
+}]);
